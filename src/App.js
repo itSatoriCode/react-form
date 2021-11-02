4556fff4f2a7c379382b90fb5684189d2851f147
MrGeet <aldarbatomunkuev@gmail.com>
2021-11-02 14:27:16 +1300

    final

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import Form from './components/Form/Form';
+import GlobalStyle, { Row, Section, Text } from './globalStyles';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
 function App() {
-	return <></>;
+	return (
+		<Router>
+			<GlobalStyle />
+			<Row gap="2rem" height="80px" justify="center" align="center" background="green">
+				<Link to="signup">
+					<Text size="2rem"> Signup</Text>
+				</Link>
+				<Link to="/">
+					<Text size="2rem"> Home</Text>
+				</Link>
+			</Row>
+			<Switch>
+				<Route path="/" exact component={() => <Section>HOME</Section>} />
+				<Route path="/signup" component={Form} />
+			</Switch>
+		</Router>
+	);
 }
 
 export default App;
--- src/globalStyles.js
+++ src/globalStyles.js
@@ -6,7 +6,7 @@ const GlobalStyle = createGlobalStyle`
    margin: 0;
    padding:0;
    box-sizing: border-box;
-	 font-family: 'Montserrat', sans-serif;
+	 font-family: 'Helvetica', sans-serif;
  }
 
 
